5cae6107c0e426b5728938a4f7f6a7856db5356d
YAGAMIL <Tablo_Jhin1996@outlook.com>
2019-04-04 18:55:25 +0800

    [master]项目框架搭建

---
+++ attendance-api/src/main/java/com/aku/attendance/api/AttendanceApiApplication.java
@@ -0,0 +1,23 @@
+package com.aku.attendance.api;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+
+/**
+ * Description:程序启动类 Created on 2019/3/18 0018 16:33
+ *
+ * @author <a href="mailto: Tablo_Jhin1996@outlook.com">Tablo</a>
+ * @version 1.0
+ */
+@EnableCaching
+@SpringBootApplication(scanBasePackages = "com.aku.attendance")
+@MapperScan("com.aku.attendance.system.dao")
+public class AttendanceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceApiApplication.class, args);
+    }
+}

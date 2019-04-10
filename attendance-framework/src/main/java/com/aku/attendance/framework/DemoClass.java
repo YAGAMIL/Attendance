555d2c71a410f495e44d700a8448c3011e3ff529
YAGAMIL <Tablo_Jhin1996@outlook.com>
2019-04-10 16:22:42 +0800

    [master]防止framework无效

---
+++ attendance-framework/src/main/java/com/aku/attendance/framework/DemoClass.java
@@ -0,0 +1,15 @@
+package com.aku.attendance.framework;
+
+/**
+ * <p>
+ * Description:没什么
+ * Program:Attendance
+ * </p>
+ * Created on 2019-04-10 16:21
+ *
+ * @author <a href="mailto: Tablo_Jhin1996@outlook.com">Tablo</a>
+ * @version 1.0
+ */
+
+public class DemoClass {
+}
